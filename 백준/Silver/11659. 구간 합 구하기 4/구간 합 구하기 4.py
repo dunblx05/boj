4953bb58f8e8b1dc941475fc91b02738934ba5d5
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-08-05 13:16:23 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 172 ms, Memory: 127128 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -4,6 +4,8 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 num = list(map(int, input().split()))
 sum = 0
+
+# 더한 값을 미리 저장할 배열
 pre_sum = [0]
 
 for i in num:
@@ -12,4 +14,5 @@ for i in num:
 
 for _ in range(m):
   l, r = map(int, input().split())
+  # 구간 합 계산
   print(pre_sum[r] - pre_sum[l - 1])
