441c90640acdecaa59df7c07ec2ebe9a43bae62d
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-08-05 14:03:52 +0900

    [Silver V] Title: 스네이크버드, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+h = list(map(int, input().split()))
+h.sort()
+
+for i in range(n):
+  if h[i] <= l:
+    l += 1
+print(l)
