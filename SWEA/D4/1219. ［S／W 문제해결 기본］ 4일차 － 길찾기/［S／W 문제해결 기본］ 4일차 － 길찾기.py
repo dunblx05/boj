217fe3ffc52a3a0244ca3e321083a72a59904d4c
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-08-06 10:52:54 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 138 ms, Memory: 45,844 KB -BaekjoonHub

--- SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
@@ -2,12 +2,15 @@ from collections import deque
 
 def bfs(graph, node, visited):
   queue = deque([node])
+
+  # 현재 노드 방문처리
   visited[node] = True
-  
+
   while queue:
     v = queue.popleft()
 
     for i in graph[v]:
+      # 방문하지 않았다면
       if visited[i] == False:
         queue.append(i)
         visited[i] = True
@@ -18,6 +21,7 @@ for tc in range(1, 11):
   graph = [[] for _ in range(100)]
   visited = [False] * 100
 
+  # 받은 리스트를 인접리스트로 변경
   for i in range(n):
     start = lst[i * 2]
     end = lst[i * 2 + 1]
