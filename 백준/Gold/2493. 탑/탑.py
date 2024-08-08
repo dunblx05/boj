b88d621b17a273a52ec7f1216269591103b1c223
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-08-08 15:41:50 +0900

    [Gold V] Title: 탑, Time: 496 ms, Memory: 99264 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -13,10 +13,11 @@ for i in range(n):
     if h[stack[-1]] >= h[i]:
       # 정답 배열에 인덱스 저장
       reception[i] = stack[-1] + 1
+      # 더 탐색할 필요 없이 break
       break
     # 현재 높이 보다 작다면 비교 안해도 되므로
     else:
-      # 스택에서 인덱스 삭제
+      # 스택에서 인덱스 삭제 후 계속 맨 위 확인
       stack.pop()
   # 스택에 인덱스를 넣어줌
   stack.append(i)
