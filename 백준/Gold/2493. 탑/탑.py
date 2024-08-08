d83f9007bce3bfbca0f46d473f724f5b943c7a3e
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-08-08 15:18:56 +0900

    [Gold V] Title: 탑, Time: 332 ms, Memory: 213576 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+h = list(map(int, input().split()))
+reception = [0 for _ in range(n)]
+stack = []
+
+for i in range(n):
+  # 스택이 있다면
+  while stack:
+    # 스택의 맨 위 높이가 현재 높이 이상이면
+    if h[stack[-1]] >= h[i]:
+      # 정답 배열에 인덱스 저장
+      reception[i] = stack[-1] + 1
+      break
+    # 현재 높이 보다 작다면 비교 안해도 되므로
+    else:
+      # 스택에서 인덱스 삭제
+      stack.pop()
+  # 스택에 인덱스를 넣어줌
+  stack.append(i)
+
+print(*reception)
