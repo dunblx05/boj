565a6ce1fbe4d471b08439582ed57a0a8a8a3d92
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-09-02 17:46:27 +0900

    [Gold III] Title: 드래곤 커브, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -1,39 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-board = [[0 for _ in range(101)] for _ in range(101)]
-result = 0
-
-# 우 상 좌 하
+# 우, 상, 좌, 하
 dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 
+n = int(input())
+board = [[0 for _ in range(101)] for _ in range(101)]
+count = 0
+
 for _ in range(n):
     sy, sx, d, g = map(int, input().split())
+    curve = [(sx, sy), (sx + dx[d], sy + dy[d])]
 
-    # 시작좌표 저장
-    curve = [(sx, sy)]
-
-    # 0세대 좌표 저장
-    curve.append((sx + dx[d], sy + dy[d]))
-
-    # 세대만큼 반복
     for _ in range(g):
         ex, ey = curve[-1]
-        #curve의 끝 좌표 기준으로 90도 회전
+
         for i in range(len(curve) - 2, -1, -1):
             cx, cy = curve[i]
-            curve.append((ex-(ey-cy), ey+(ex-cx)))
+            curve.append((ex - (ey - cy), ey + (ex - cx)))
 
-    # 좌표리스트에서 좌표 확인 후 board에 표시
     for i, j in curve:
         board[i][j] = 1
 
-# 정사각형 검사
 for i in range(100):
     for j in range(100):
-        if board[i][j] and board[i+1][j] and board[i][j+1] and board[i+1][j+1]:
-            result += 1
+        if board[i][j] == 1 and board[i+1][j] == 1 and board[i][j+1] == 1 and board[i+1][j+1] == 1:
+            count += 1
 
-print(result)
+print(count)
