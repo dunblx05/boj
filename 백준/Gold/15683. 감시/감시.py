fc42bf00d8578f2e861cb4039d13c11fb6cad6bc
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-09-04 11:24:11 +0900

    [Gold III] Title: 감시, Time: 2456 ms, Memory: 31684 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,9 +1,15 @@
 import copy
 import sys
+
 input = sys.stdin.readline
 
-def fill(board, mode, x, y):
-    for i in mode:
+# 상, 우, 하, 좌
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+def fill(x, y, dir, board):
+    for i in dir:
         nx = x
         ny = y
 
@@ -18,48 +24,49 @@ def fill(board, mode, x, y):
             elif board[nx][ny] == 0:
                 board[nx][ny] = -1
 
+    return
+
+
 def dfs(depth, board):
     global ans
-    
-    if depth == len(cctv):
-        count_area = 0
+    # 종료조건 : 모든 cctv 확인했다면
+    if depth == len(info):
+        count = 0
         for i in range(n):
-            count_area += board[i].count(0)
-        ans = min(ans, count_area)
+            count += board[i].count(0)
+        if count < ans:
+            ans = count
         return
 
-    new_office = copy.deepcopy(board)
-    cctv_num, x, y = cctv[depth]
+    x, y, cctv_num = info[depth]
+    new_board = copy.deepcopy(board)
 
-    for i in mode[cctv_num]:
-        fill(new_office, i, x, y)
-        dfs(depth + 1, new_office)
-        new_office = copy.deepcopy(board)
+    for i in cctv[cctv_num]:
+        fill(x, y, i, new_board)
+        dfs(depth + 1, new_board)
+        new_board = copy.deepcopy(board)
 
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-ans = sys.maxsize
 n, m = map(int, input().split())
 office = [list(map(int, input().split())) for _ in range(n)]
+info = []
+ans = sys.maxsize
 
-cctv = []
-
-# cctv의 방향 정보
-mode = [
+# cctv 방향 정보
+cctv = [
     [],
     [[0], [1], [2], [3]],
     [[0, 2], [1, 3]],
     [[0, 1], [1, 2], [2, 3], [0, 3]],
-    [[0, 1, 2], [0, 1, 3], [1, 2, 3], [0, 2, 3]],
+    [[0, 1, 2], [1, 2, 3], [0, 2, 3], [0, 1, 3]],
     [[0, 1, 2, 3]]
 ]
 
 for i in range(n):
     for j in range(m):
-        if office[i][j] in [1, 2, 3, 4, 5]:
-            cctv.append([office[i][j], i, j])
+        if 1 <= office[i][j] <= 5:
+            info.append([i, j, office[i][j]])
 
 dfs(0, office)
+
 print(ans)
