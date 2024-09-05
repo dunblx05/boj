870ad2ddf14efad4fc80f175d8cb7325f480e0b7
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-09-05 10:41:35 +0900

    [Gold III] Title: 게리맨더링, Time: 132 ms, Memory: 114100 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.py
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.py
@@ -1,50 +1,54 @@
 from itertools import combinations
 from collections import deque
 import sys
+
 input = sys.stdin.readline
 
-def bfs(nodes):
-  queue = deque()
-  visited = [False for _ in range(n + 1)]
-  queue.append(nodes[0])
-  visited[nodes[0]] = True
-  count = 1
-  total = 0
-  
-  while queue:
-    x = queue.popleft()
-    total += population[x]
-    
-    for i in info[x]:
-      if visited[i] == False and i in nodes:
-        queue.append(i)
-        visited[i] = True
-        count += 1
-  
-  return total, count
+
+def bfs(c):
+    queue = deque()
+    queue.append(c[0])
+
+    visited = [False for _ in range(n + 1)]
+    visited[c[0]] = True
+
+    total = 0
+    count = 1
+
+    while queue:
+        node = queue.popleft()
+        total += population[node]
+
+        for i in cityInfo[node]:
+            if visited[i] is False and i in c:
+                queue.append(i)
+                visited[i] = True
+                count += 1
+
+    return total, count
+
 
 n = int(input())
 population = [0] + list(map(int, input().split()))
-info = [[]]
-city = [i for i in range(1, n + 1)]
+cityInfo = [[]]
+for i in range(1, n + 1):
+    tempInput = list(map(int, input().split()))
+    cityInfo.append(tempInput[1:])
 
+cities = [i for i in range(1, n + 1)]
 ans = sys.maxsize
 
-for i in range(n):
-  tmp = list(map(int, input().split()))
-  info.append(tmp[1:])
-
 for i in range(1, n // 2 + 1):
-  comb = list(combinations(city, i))
-  
-  for c in comb:
-    total1, cnt1 = bfs(c)
-    total2, cnt2 = bfs([i for i in range(1, n + 1) if i not in c])
-    
-    if cnt1 + cnt2 == n:
-      ans = min(ans, abs(total1 - total2))
-      
+    for comb in list(combinations(cities, i)):
+        pop1, cnt1 = bfs(comb)
+        pop2, cnt2 = bfs([i for i in range(1, n + 1) if i not in comb])
+
+        if cnt1 + cnt2 == n:
+            res = abs(pop1 - pop2)
+            if res < ans:
+                ans = res
+
 if ans == sys.maxsize:
-  print(-1)
+    print(-1)
 else:
-  print(ans)
+    print(ans)
