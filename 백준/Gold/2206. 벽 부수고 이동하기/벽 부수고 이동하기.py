6172e0ba1f0400c2f0174157b90b985cf11eb047
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-09-05 13:55:44 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 724 ms, Memory: 223672 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,5 +1,4 @@
 from collections import deque
-from copy import deepcopy
 import sys
 
 input = sys.stdin.readline
@@ -11,13 +10,16 @@ dy = [0, 1, 0, -1]
 def bfs():
     queue = deque()
     queue.append((0, 0, 0))
-    visited[0][0][0] = 1
+    visited_no_break[0][0] = 1
 
     while queue:
         x, y, break_wall = queue.popleft()
 
         if x == n - 1 and y == m - 1:
-            return visited[x][y][break_wall]
+            if break_wall == 0:
+                return visited_no_break[x][y]
+            else:
+                return visited_break[x][y]
 
         for i in range(4):
             nx = x + dx[i]
@@ -25,13 +27,20 @@ def bfs():
 
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            # 방문안했고 0인경우
-            if visited[nx][ny][break_wall] == 0 and board[nx][ny] == 0:
-                visited[nx][ny][break_wall] = visited[x][y][break_wall] + 1
-                queue.append((nx, ny, break_wall))
-            # 방문안했는데 벽을 만나고 한번도 안 부숨
-            elif break_wall == 0 and board[nx][ny] == 1:
-                visited[nx][ny][1] = visited[x][y][0] + 1
+
+            # 벽을 부수지 않고 방문하는 경우
+            if break_wall == 0 and visited_no_break[nx][ny] == 0 and board[nx][ny] == 0:
+                visited_no_break[nx][ny] = visited_no_break[x][y] + 1
+                queue.append((nx, ny, 0))
+
+            # 벽을 부수고 방문하는 경우
+            if break_wall == 1 and visited_break[nx][ny] == 0 and board[nx][ny] == 0:
+                visited_break[nx][ny] = visited_break[x][y] + 1
+                queue.append((nx, ny, 1))
+
+            # 벽을 부수는 경우 (한 번도 부수지 않았을 때만)
+            if break_wall == 0 and board[nx][ny] == 1 and visited_break[nx][ny] == 0:
+                visited_break[nx][ny] = visited_no_break[x][y] + 1
                 queue.append((nx, ny, 1))
 
     return -1
@@ -39,6 +48,7 @@ def bfs():
 
 n, m = map(int, input().split())
 board = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+visited_no_break = [[0] * m for _ in range(n)]  # 벽을 부수지 않고 방문했을 때
+visited_break = [[0] * m for _ in range(n)]     # 벽을 부수고 방문했을 때
 
 print(bfs())
