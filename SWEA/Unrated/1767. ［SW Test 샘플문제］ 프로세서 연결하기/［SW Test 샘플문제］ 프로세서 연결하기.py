1d75b47b53b3b29fb6b3a8ea27b61ac4071de672
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-09-06 16:00:49 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 2,830 ms, Memory: 86,456 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.py
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.py
@@ -1,86 +1,83 @@
-# import copy
+def connect_wire(x, y, arr, direction):
+    canConnect = True
+    nx = x
+    ny = y
 
-def connectWire(x, y, i, graph):
-   connect = True
-   nx = x
-   ny = y
-   
-   while 1:
-      nx += dx[i]
-      ny += dy[i]
-      
-      if nx < 0 or ny < 0 or nx >= n or ny >= n:
-         break
-      if graph[nx][ny] != 0:
-         connect = False
-         break
+    while 1:
+        nx += dx[direction]
+        ny += dy[direction]
 
-   if connect:
-      count = 0
-      nx = x
-      ny = y
-      
-      while 1:
-         nx += dx[i]
-         ny += dy[i]
-         
-         if nx < 0 or ny < 0 or nx >= n or ny >= n:
+        if nx < 0 or ny < 0 or nx >= n or ny >= n:
             break
-         
-         graph[nx][ny] = 2
-         count += 1
-      return count
-   
-   else:
-      return -1
+        if arr[nx][ny] != 0:
+            canConnect = False
+            break
+    if canConnect == True:
+        count = 0
+        nx = x
+        ny = y
 
-def backTracking(index, cnt, minWire, graph):
-   global ansCore
-   global ansWire
-   
-   if index == len(core):
-      if ansCore < cnt:
-         ansCore = cnt
-         ansWire = minWire
-         
-      elif ansCore == cnt:
-         ansWire = min(minWire, ansWire)
-         
-      return
-   
-   tempGraph1 = [i[:] for i in graph]
-   
-   for i in range(4):
-      tempGraph2 = [i[:] for i in graph]
-      wire = connectWire(core[index][0], core[index][1], i, tempGraph2)
-      
-      if wire != -1:
-         backTracking(index + 1, cnt + 1, minWire + wire, tempGraph2)
-   
-   backTracking(index + 1, cnt, minWire, tempGraph1)
-   
-   return
-   
-t = int(input())
+        while 1:
+            nx += dx[direction]
+            ny += dy[direction]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                break
+
+            arr[nx][ny] = 2
+            count += 1
+        return count
+    else:
+
+        return -1
+
+
+def backtracking(index, core_count, wire_count, arr):
+    global answer_core
+    global answer_wire
+
+    # 모든 코어 확인
+    if index == len(core_list):
+        if core_count > answer_core:
+            answer_core = core_count
+            answer_wire = wire_count
+
+        elif core_count == answer_core:
+            if answer_wire > wire_count:
+                answer_wire = wire_count
+        return
+
+    temp_board = [tmp[:] for tmp in arr]
+
+    for i in range(4):
+        temp_board2 = [tmp[:] for tmp in arr]
+        wire = connect_wire(core_list[index][0], core_list[index][1], temp_board2, i)
+
+        if wire != -1:
+            backtracking(index + 1, core_count + 1, wire_count + wire, temp_board2)
+
+    backtracking(index + 1, core_count, wire_count, temp_board)
+
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+t = int(input())
+
 for tc in range(1, t + 1):
-   n = int(input())
-   arr = [list(map(int, input().split())) for _ in range(n)]
-   
-   visited = [[False for _ in range(n)] for _ in range(n)]
-   
-   core = []
-   ansCore = 0
-   ansWire = 0
-   
-   for i in range(1, n - 1):
-      for j in range(1, n - 1):
-         if arr[i][j] == 1:
-            core.append((i, j))
-   
-   backTracking(0, 0, 0, arr)
-   
-   print(f"#{tc} {ansWire}")
+    n = int(input())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    core_list = []
+    # 정답 코어 개수
+    answer_core = 0
+    # 정답 전선 개수
+    answer_wire = 0
+
+    for i in range(1, n - 1):
+        for j in range(1, n - 1):
+            if board[i][j] == 1:
+                core_list.append((i, j))
+
+    backtracking(0, 0, 0, board)
+
+    print(f"#{tc} {answer_wire}")
