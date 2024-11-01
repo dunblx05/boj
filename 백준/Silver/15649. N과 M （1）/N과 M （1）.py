fafd2ae6212032245164ef170d7090eda0a1f78a
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-11-01 09:05:29 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 123868 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,23 @@
 import sys
+import copy
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-s = []
-def dfs():
-    if len(s) == m:
-        print(' '.join(map(str, s)))
+pers = []
 
-    for i in range(1, n+1):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
-dfs()
+def backtracking(n, m):
+  if len(per) == m:
+    pers.append(copy.deepcopy(per))
+
+  for i in range(1, n + 1):
+    if i not in per:
+      per.append(i)
+      backtracking(n, m)
+      per.pop()
+
+n, m = map(int, input().split())
+per = []
+
+backtracking(n, m)
+
+for i in range(len(pers)):
+  print(*pers[i])
