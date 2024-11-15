99aab5839d3f1113ad9919aa7f27fe157e47c9e3
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-11-16 02:33:10 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 4 ms, Memory: 2280 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+const int INF = 1e9;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int n;
+int cave[125][125];
+int visited[125][125];
+int answer;
+
+struct Link {
+    int x;
+    int y;
+    int money;
+};
+
+struct Comp {
+    bool operator()(const Link &a, const Link &b) const {
+        return a.money > b.money;
+    }
+};
+
+void dijkstra() {
+    priority_queue<Link, vector<Link>, Comp> pq;
+    pq.push({0, 0, cave[0][0]});
+
+    visited[0][0] = cave[0][0];
+
+    while (!pq.empty()) {
+        auto cur = pq.top();
+        pq.pop();
+
+        int x = cur.x;
+        int y = cur.y;
+        int money = cur.money;
+
+        if (money > visited[x][y]) {
+            continue;
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                continue;
+            }
+
+            int nMoney = cave[nx][ny] + money;
+
+            // 더 작은 값으로 이동할 수 있다면
+            if (visited[nx][ny] > nMoney) {
+                visited[nx][ny] = nMoney;
+                pq.push({nx, ny, nMoney});
+            }
+        }
+    }
+
+    answer = visited[n - 1][n - 1];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tc = 0;
+
+    while (true) {
+        cin >> n;
+
+
+        if (n == 0) {
+            break;
+        }
+
+        tc++;
+        memset(cave, 0, sizeof(cave));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cin >> cave[i][j];
+                visited[i][j] = INF;
+            }
+        }
+
+        dijkstra();
+
+        cout << "Problem" << " " << tc << ": " << answer << endl;
+    }
+}
