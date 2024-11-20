7bc7e8ceaa5e181cbe03c80eaf8f3a79c17ed506
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-11-21 00:40:30 +0900

    [Gold V] Title: 강의실 배정, Time: 56 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <queue>
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+
+int n;
+vector<pair<int, int> > lesson;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    priority_queue<int, vector<int>, greater<int> > pq;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int s, e;
+        cin >> s >> e;
+
+        lesson.push_back({s, e});
+    }
+
+    sort(lesson.begin(), lesson.end());
+
+    pq.push(lesson[0].first);
+
+    for (auto &i: lesson) {
+        pq.push(i.second);
+
+        if (pq.top() <= i.first) {
+            pq.pop();
+        }
+    }
+
+    cout << pq.size();
+}
