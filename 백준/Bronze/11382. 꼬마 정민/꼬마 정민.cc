55b6a97e2cef73170b8a0ba080f864c10ba06069
김두영 <33441610+dunblx05@users.noreply.github.com>
2024-12-14 04:03:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <queue>
+#include <cstdio>
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+}
