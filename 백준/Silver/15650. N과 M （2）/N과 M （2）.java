16faf6d922e3ef88aa2d5dda73a3fbc9bca41f32
김두영 <33441610+dunblx05@users.noreply.github.com>
2025-05-13 01:01:07 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -5,37 +5,38 @@ public class Main {
 
     static int n, m;
     static int[] arr;
-    static int[] cur;
+    static int[] comb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n];
-        cur = new int[m];  // ✅ 여기서 제대로 m으로 크기 맞춰 초기화
-
+        comb = new int[m];
         for (int i = 0; i < n; i++) {
             arr[i] = i + 1;
         }
-
         combination(0, 0);
     }
 
     static void combination(int depth, int start) {
         if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+
             for (int i = 0; i < m; i++) {
-                System.out.print(cur[i] + " ");
+                sb.append(comb[i] + " ");
             }
-            System.out.println();
+            System.out.println(sb);
             return;
         }
 
         for (int i = start; i < n; i++) {
-            cur[depth] = arr[i];
+            comb[depth] = arr[i];
             combination(depth + 1, i + 1);
         }
     }
+
 }
