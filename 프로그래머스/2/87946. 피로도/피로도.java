feca88d9a14bfd5a9bd54919194595bd8d64293e
김두영 <33441610+dunblx05@users.noreply.github.com>
2025-05-13 13:15:17 +0900

    [level 2] Title: 피로도, Time: 47.16 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    
+    private static List<int[][]> result;
+    private static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        visited = new boolean[dungeons.length];
+        result = new ArrayList<>();
+        // 현재 순열 저장 배열
+        int[][] cur = new int[dungeons.length][2];
+        
+        permutation(dungeons, cur, 0);
+        
+        for(int[][] perm : result) {
+            int tired = k;
+            int count = 0;
+            
+            for(int[] p : perm) {
+                if(tired >= p[0]) {
+                    tired -= p[1];
+                    count++;
+                }
+            }
+            if(count >= answer) {
+                answer = count;
+            }
+            
+        }
+        return answer;
+    }
+    
+    private static void permutation(int dungeons[][], int[][] cur, int length) {
+        if (length == dungeons.length) {
+            // 완성된 순열 복사해서 저장
+            int[][] copy = new int[dungeons.length][2];
+            for (int i = 0; i < dungeons.length; i++) {
+                copy[i][0] = cur[i][0];
+                copy[i][1] = cur[i][1];
+            }
+            result.add(copy);
+            return;
+        }
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            if(visited[i] == true) {
+                continue;
+            }
+            visited[i] = true;
+            cur[length] = dungeons[i];
+            permutation(dungeons, cur, length + 1);
+            visited[i] = false;
+        }
+    }
+}
