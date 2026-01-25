dbaf2addcb4de647edb917a494ffd97d7fc0f060
김두영 <33441610+dunblx05@users.noreply.github.com>
2026-01-25 16:09:42 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int[][] arr = new int[101][101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 색종이의 개수 N 읽기
+        N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (arr[j][k] == 0) {
+                        arr[j][k] = 1;
+                    }
+                }
+            }
+
+        }
+
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                if (arr[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        sb.append(count);
+
+        System.out.println(sb);
+
+    }
+}
